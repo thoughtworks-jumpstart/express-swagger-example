7a471aa2bb0b8084478f669cc24a3d9707458711
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-03 14:07:55 +0800

    Server Swagger docs using swagger-ui-express

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-sample.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => res.json({ message: "Welcome to express!" }));
 
